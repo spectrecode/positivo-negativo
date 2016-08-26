aed50d5e83ba92a2bed612e6b538adf27a9986b1
spectrecode <you@example.com>
2016-08-26 10:28:50 -0500

    aprendiendo positivos negativos

---
+++ main.js
@@ -0,0 +1,8 @@
+var num = prompt ("ingrese un número");
+var numero = parseInt(num);
+	if (numero > 0) {
+		alert("El número ingresado es positivo");
+	}
+	else {
+		alert("El número ingresado es negativo");
+	}
---
+++ readme.md
@@ -0,0 +1,8 @@
+#POSITIVO Y NEGATIVO
+* inicio
+* leer datos
+* verificar si es positivo o negativo
+* dar respuesta
+* fin
+
+![Sin titulo](img/ej1.jpg)
